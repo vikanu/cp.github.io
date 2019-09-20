f8c55a1600be5199aa846cc937c7ddf3bd9beef3
vikanu <vikanuraini1@gmail.com>
2019-09-20 13:37:11 +0700

    first commit

---
+++ precache-manifest.10a32d4443fcfbb888b3bfc73ef08cce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7375a36d3a45d8ac8c7e",
+    "url": "/js/chunk-vendors.d4fb9efb.js"
+  },
+  {
+    "revision": "0c503af06d500dd7aaef",
+    "url": "/js/app.4a3d322c.js"
+  },
+  {
+    "revision": "2abf005ca5f28591b8eb",
+    "url": "/js/about.6a697ed5.js"
+  },
+  {
+    "revision": "4ba4733f8560e66badf2750f9084395c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0c503af06d500dd7aaef",
+    "url": "/css/app.5584eb01.css"
+  },
+  {
+    "revision": "2abf005ca5f28591b8eb",
+    "url": "/css/about.0dd205aa.css"
+  }
+];
